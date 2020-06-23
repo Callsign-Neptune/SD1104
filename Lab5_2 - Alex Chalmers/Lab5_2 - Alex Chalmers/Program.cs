45bc51174526b8e402012cc46e088cb5f28d1862
Callsign-Neptune <66689267+Callsign-Neptune@users.noreply.github.com>
2020-06-23 10:00:59 -0500

    Add files via upload

---
+++ Lab5_2 - Alex Chalmers/Lab5_2 - Alex Chalmers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab5_2___Alex_Chalmers
+{
+    class Program
+    {
+        public enum EndsInY
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+        static void Main(string[] args)
+        {
+            List<string> taskList = new List<string>();
+            foreach (EndsInY day in Enum.GetValues(typeof(EndsInY)))
+            {
+                Console.WriteLine("New task for" + day);
+                string task = day + ":" + Console.ReadLine();
+                taskList.Add(task);
+            }
+            foreach(string task in taskList)
+            {
+                Console.WriteLine(task);
+            }
+        }
+    }
+}
