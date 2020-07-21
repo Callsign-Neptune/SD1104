f3b15f5cd251eb5e774a671309ad4ef393a87a0f
Callsign-Neptune <66689267+Callsign-Neptune@users.noreply.github.com>
2020-07-21 07:40:16 -0500

    Add files via upload

---
+++ Lab4_6 - Alex Chalmers/Lab4_6 - Alex Chalmers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Lab4_6___Alex_Chalmers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] weekDays = new string[] { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+            string[] toDo = new string[7];
+
+            for (int i = 0; i < weekDays.Length; i++)
+            {
+                Console.WriteLine("Create a task for " + weekDays[i]);
+                toDo[i] = weekDays[i] + ": " + Console.ReadLine();
+            }
+
+            for (int i = 0; i < weekDays.Length; i++)
+            {
+                Console.WriteLine(toDo[i]);
+            }
+        }
+    }
+}
